fb967f3ee2f7bb385f2dc98f629b22d3ef4a91dd
Franz Andel <franz.andel@ovo.id>
2022-07-16 17:51:55 +0700

    feat(remote-mediator): Integrate Remote Mediator to apps

---
+++ app/src/main/java/com/franzandel/dicodingintermediatesubmission/data/model/RemoteKeysEntity.kt
@@ -0,0 +1,19 @@
+package com.franzandel.dicodingintermediatesubmission.data.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Created by Franz Andel <franz.andel@ovo.id>
+ * on 12 July 2022.
+ */
+
+@Entity(tableName = "remote_keys")
+data class RemoteKeysEntity(
+    @PrimaryKey val id: String,
+    @ColumnInfo(name = "prev_key")
+    val prevKey: Int?,
+    @ColumnInfo(name = "next_key")
+    val nextKey: Int?
+)
--- app/src/main/java/com/franzandel/dicodingintermediatesubmission/data/remotemediator/HomeRemoteMediator.kt
+++ app/src/main/java/com/franzandel/dicodingintermediatesubmission/data/remotemediator/HomeRemoteMediator.kt
@@ -4,8 +4,11 @@ import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadType
 import androidx.paging.PagingState
 import androidx.paging.RemoteMediator
+import androidx.room.withTransaction
+import com.franzandel.dicodingintermediatesubmission.base.data.NetworkObject
 import com.franzandel.dicodingintermediatesubmission.data.database.StoriesDatabase
-import com.franzandel.dicodingintermediatesubmission.data.model.HomeEntity
+import com.franzandel.dicodingintermediatesubmission.data.mapper.HomeResponseMapper
+import com.franzandel.dicodingintermediatesubmission.data.model.RemoteKeysEntity
 import com.franzandel.dicodingintermediatesubmission.data.model.StoryEntity
 import com.franzandel.dicodingintermediatesubmission.data.service.HomeService
 
@@ -14,15 +17,17 @@ import com.franzandel.dicodingintermediatesubmission.data.service.HomeService
  * on 09 July 2022.
  */
 
-@ExperimentalPagingApi
+@OptIn(ExperimentalPagingApi::class)
 class HomeRemoteMediator(
     private val database: StoriesDatabase,
-    private val apiService: HomeService
+    private val apiService: HomeService,
+    private val token: String
 ) : RemoteMediator<Int, StoryEntity>() {
 
     private companion object {
         const val INITIAL_PAGE_INDEX = 1
     }
+
     override suspend fun initialize(): InitializeAction {
         return InitializeAction.LAUNCH_INITIAL_REFRESH
     }
@@ -31,32 +36,72 @@ class HomeRemoteMediator(
         loadType: LoadType,
         state: PagingState<Int, StoryEntity>
     ): MediatorResult {
-        val page = INITIAL_PAGE_INDEX
+        val page = when (loadType) {
+            LoadType.REFRESH -> {
+                val remoteKeys = getRemoteKeyClosestToCurrentPosition(state)
+                remoteKeys?.nextKey?.minus(1) ?: INITIAL_PAGE_INDEX
+            }
+            LoadType.PREPEND -> {
+                val remoteKeys = getRemoteKeyForFirstItem(state)
+                val prevKey = remoteKeys?.prevKey
+                    ?: return MediatorResult.Success(endOfPaginationReached = remoteKeys != null)
+                prevKey
+            }
+            LoadType.APPEND -> {
+                val remoteKeys = getRemoteKeyForLastItem(state)
+                val nextKey = remoteKeys?.nextKey
+                    ?: return MediatorResult.Success(endOfPaginationReached = remoteKeys != null)
+                nextKey
+            }
+        }
+
         try {
-            val responseData = apiService.getStories("", page, state.config.pageSize)
-            val endOfPaginationReached = responseData.isEmpty()
+            val storiesResponse =
+                apiService.getStories(NetworkObject.wrapBearer(token), page, state.config.pageSize)
+            val endOfPaginationReached = storiesResponse.listStory.isEmpty()
+
             database.withTransaction {
                 if (loadType == LoadType.REFRESH) {
                     database.homeDao().deleteAll()
+                    database.remoteKeysDao().deleteRemoteKeys()
                 }
-                val homeEntity = HomeEntity(
-                    responseData.listStory.map {
-                        StoryEntity(
-                            it.id,
-                            it.createdAt,
-                            it.description,
-                            it.lat,
-                            it.lon,
-                            it.name,
-                            it.photoUrl
-                        )
-                    }
+
+                val prevKey = if (page == 1) null else page - 1
+                val nextKey = if (endOfPaginationReached) null else page + 1
+                val remoteKeysEntities = HomeResponseMapper.transformRemoteKeysEntities(
+                    storiesResponse.listStory,
+                    prevKey,
+                    nextKey
                 )
-                database.homeDao().insertAll(homeEntity)
+                database.remoteKeysDao().insertAll(remoteKeysEntities)
+
+                val storyEntities =
+                    HomeResponseMapper.transformStoryEntities(storiesResponse.listStory)
+                database.homeDao().insertAll(storyEntities)
             }
             return MediatorResult.Success(endOfPaginationReached = endOfPaginationReached)
         } catch (exception: Exception) {
             return MediatorResult.Error(exception)
+        }
+    }
+
+    private suspend fun getRemoteKeyForLastItem(state: PagingState<Int, StoryEntity>): RemoteKeysEntity? {
+        return state.pages.lastOrNull { it.data.isNotEmpty() }?.data?.lastOrNull()?.let { data ->
+            database.remoteKeysDao().getRemoteKeysId(data.id)
+        }
+    }
+
+    private suspend fun getRemoteKeyForFirstItem(state: PagingState<Int, StoryEntity>): RemoteKeysEntity? {
+        return state.pages.firstOrNull { it.data.isNotEmpty() }?.data?.firstOrNull()?.let { data ->
+            database.remoteKeysDao().getRemoteKeysId(data.id)
+        }
+    }
+
+    private suspend fun getRemoteKeyClosestToCurrentPosition(state: PagingState<Int, StoryEntity>): RemoteKeysEntity? {
+        return state.anchorPosition?.let { position ->
+            state.closestItemToPosition(position)?.id?.let { id ->
+                database.remoteKeysDao().getRemoteKeysId(id)
+            }
         }
     }
 }
