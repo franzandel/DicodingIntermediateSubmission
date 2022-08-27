8d962db213e14aebe2c18478378b48e01cb923d8
Franz Andel <franz.andel@ovo.id>
2022-08-27 16:35:18 +0700

    test(home-dao): Add test cases

---
+++ app/src/androidTest/java/com/franzandel/dicodingintermediatesubmission/data/dao/HomeDaoTest.kt
@@ -0,0 +1,88 @@
+package com.franzandel.dicodingintermediatesubmission.data.dao
+
+import androidx.paging.PagingSource
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.franzandel.dicodingintermediatesubmission.data.database.StoriesDatabase
+import com.franzandel.dicodingintermediatesubmission.test.RoomUtils
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Created by Franz Andel
+ * on 27 August 2022.
+ */
+
+@RunWith(AndroidJUnit4::class)
+class HomeDaoTest {
+
+    private lateinit var database: StoriesDatabase
+    private lateinit var dao: HomeDao
+    private val storyEntities = RoomUtils.getStoryEntities()
+
+    @Before
+    fun initDb() {
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            StoriesDatabase::class.java
+        ).build()
+        dao = database.homeDao()
+    }
+
+    @After
+    fun closeDb() {
+        database.close()
+    }
+
+    @Test
+    fun saveStoryEntities_success() {
+        runBlocking {
+            dao.insertAll(storyEntities)
+
+            val expected = PagingSource.LoadResult.Page(
+                data = storyEntities,
+                prevKey = null,
+                nextKey = null,
+                itemsBefore = 0,
+                itemsAfter = 0
+            )
+            val actual = dao.getAll().load(
+                PagingSource.LoadParams.Refresh(
+                    key = null,
+                    loadSize = 1,
+                    placeholdersEnabled = false
+                )
+            )
+            Assert.assertEquals(expected, actual)
+        }
+    }
+
+    @Test
+    fun deleteStoryEntities_success() {
+        runBlocking {
+            dao.insertAll(storyEntities)
+            dao.deleteAll()
+
+            val expected = PagingSource.LoadResult.Page(
+                data = listOf(),
+                prevKey = null,
+                nextKey = null,
+                itemsBefore = 0,
+                itemsAfter = 0
+            )
+            val actual = dao.getAll().load(
+                PagingSource.LoadParams.Refresh(
+                    key = null,
+                    loadSize = 1,
+                    placeholdersEnabled = false
+                )
+            )
+            Assert.assertEquals(expected, actual)
+        }
+    }
+}
